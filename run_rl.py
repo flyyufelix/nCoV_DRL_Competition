1be8e0bbed7099f999b861c11c0c928518c66e5e
Felix Yu <flyyufelix@gmail.com>
2020-03-01 19:51:55 +0000

    Edits in run_rl

--- run_rl.py
+++ run_rl.py
@@ -27,8 +27,6 @@ tokens = ['<', '>', '#', '%', ')', '(', '+', '-', '/', '.', '1', '0', '3', '2', 
           '6', '9', '8', '=', 'A', '@', 'C', 'B', 'F', 'I', 'H', 'O', 'N', 'P', 'S', '[', ']',
           '\\', 'c', 'e', 'i', 'l', 'o', 'n', 'p', 's', 'r', '\n']
 
-print("Parsing Data...")
-
 gen_data = GeneratorData(training_data_path=gen_data_path, delimiter='\t', 
                          cols_to_read=[0], keep_header=True, tokens=tokens)
 
@@ -42,7 +40,7 @@ optimizer_instance = torch.optim.Adadelta
 
 from reinforcement import Reinforcement
 
-my_generator_max = StackAugmentedRNN(input_size=gen_data.n_characters, 
+my_generator = StackAugmentedRNN(input_size=gen_data.n_characters, 
                                      hidden_size=hidden_size,
                                      output_size=gen_data.n_characters, 
                                      layer_type=layer_type,
@@ -53,18 +51,17 @@ my_generator_max = StackAugmentedRNN(input_size=gen_data.n_characters,
 
 model_path = './checkpoints/generator/checkpoint_biggest_rnn'
 
-my_generator_max.load_model(model_path)
+my_generator.load_model(model_path)
 
 # Params for RL
-n_to_generate = 200
-n_policy_replay = 10
 n_policy = 30
-n_iterations = 60
+n_iterations = 100
+n_batch = 10
 
-def simple_moving_average(previous_values, new_value, ma_window_size=10):
-    value_ma = np.sum(previous_values[-(ma_window_size-1):]) + new_value
-    value_ma = value_ma/(len(previous_values[-(ma_window_size-1):]) + 1)
-    return value_ma
+top_ligands_file = "top_ligands.txt"
+
+if os.path.exists(top_ligands_file):
+    os.remove(top_ligands_file)
 
 def get_docking_reward(smile):
     """
@@ -75,7 +72,7 @@ def get_docking_reward(smile):
     vina_cmd = ['./vina','--receptor','receptor.pdbqt','--ligand','ligand.pdbqt','--log','dock.log','--exhaustiveness','3','--center_x','-10','--center_y','10','--center_z','70','--size_x','10','--size_y','15','--size_z','15']
 
     # Check SMILE validity
-    binding_score = 0 # Penalty for invalid SMILES
+    binding_score = -1.0 # Penalty for invalid SMILES
     if Chem.MolFromSmiles(smile) is None:
         print("Invalid SMILE: " + str(smile))
         return binding_score
@@ -102,31 +99,39 @@ def get_docking_reward(smile):
         except:
             pass
 
+    # Strengthen the reward for good binding score
     reward = 0.0
     if binding_score > 6.0:
-        reward = 30.0
+        reward = 50.0
     elif binding_score > 5.5:
-        reward = 20.0
+        reward = 40.0
     elif binding_score > 5.0:
-        reward = 10.0
+        reward = 30.0
     elif binding_score > 4.5:
-        reward = 5.0
+        reward = 10.0
     elif binding_score > 0.0:
-        reward = 1.0
+        reward = -1.0
+
+    # Save ligand with reward >= 5.0
+    if binding_score > 4.5:
+        with open(top_ligands_file, "a+") as f:
+            f.write(smile + "," + str(binding_score) + "," + str(reward) + "\n")
 
     print("Binding Score: " + str(binding_score) + " Reward: " + str(reward))
     return reward
 
-RL_agent = Reinforcement(my_generator_max, get_docking_reward)
+def simple_moving_average(previous_values, new_value, ma_window_size=10):
+    value_ma = np.sum(previous_values[-(ma_window_size-1):]) + new_value
+    value_ma = value_ma/(len(previous_values[-(ma_window_size-1):]) + 1)
+    return value_ma
+
+RL_agent = Reinforcement(my_generator, get_docking_reward)
 
 rewards = []
-rl_losses = []
 
 for i in range(n_iterations):
     for j in trange(n_policy, desc='Policy gradient...'):
-        cur_reward, cur_loss = RL_agent.policy_gradient(gen_data)
+        cur_reward, cur_loss = RL_agent.policy_gradient(gen_data, n_batch)
         rewards.append(simple_moving_average(rewards, cur_reward)) 
         print("Rewards: " + str(rewards))
-        rl_losses.append(simple_moving_average(rl_losses, cur_loss))
-        print("Losses: " + str(rl_losses))
 
